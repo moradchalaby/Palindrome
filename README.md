fcf9d336f3bf3a5d057b36b54d6e33d76a188762
orinch <moradchalaby@gmail.com>
2023-11-03 05:48:50 +0300

    last

---
+++ README.md
@@ -0,0 +1,37 @@
+# Palindrom Sınıfı
+
+Bu PHP sınıfı, bir kelimenin veya paragraftaki palindrom kelime ve kelime dizilerini bulmanıza olanak sağlar.
+
+## Kullanım
+
+```php
+require 'Palindrom.php';
+
+$palindrom = new Palindrom();
+
+// Tek bir kelimenin palindrom olup olmadığını kontrol etmek için
+$is_palindrome = $palindrom->palindrom_mu("ana");
+
+// Bir paragraftaki palindromları bulmak için
+$paragraph = "Ara; kazakla kayak arası 12321.";
+$palindrome_results = $palindrom->palindrom_paragraf($paragraph);
+```
+
+## Fonksiyonlar
+```php
+palindrom_mu($kelime)
+```
+Verilen kelimenin bir palindrom olup olmadığını kontrol eder.
+
+```$kelime```: Kontrol edilecek kelime.
+```php
+palindrom_paragraf($paragraf)
+````
+
+
+Verilen paragraftaki palindrom kelimeleri ve kelime dizilerini bulur.
+
+
+```$paragraf```: Palindrom aranacak paragraf.
+## Lisans
+Bu proje MIT lisansı altında lisanslanmıştır. Daha fazla bilgi için [LİSANS](LICENSE) dosyasına göz atın.
--- palindrom.php
+++ palindrom.php
@@ -82,16 +82,6 @@ class Palindrom
 
 
 
-$paragraf = "Ada, ala, efe, ata, ana, kabak gibi a birçok palindrom kelime Türkçe dilinde mevcuttur. Ayrıca bazı palindrom cümleler de ilgi çekicidir. Örneğin 'Arazi küçük iz ara' gibi cümleler hem anlamlı hem de palindromdur. Ancak 'Ah adi demir erimedi daha' cümlesindeki 'adi' kelimesi palindrom değildir.
-
-Palindromları bulmak eğlenceli bir dil oyunudur. 'Kazak' kelimesini tersinden okuduğunuzda da aynı sonucu alırsınız. Ayrıca bazı palindrom kelimelerin baş harflerini sıralayarak da yeni palindromlar oluşturabilirsiniz. Örneğin 'Efe' kelimesini bu şekilde oluşturabilirsiniz.
-
-Sonuç olarak, palindromlar Türkçe dilinde de oldukça ilginç ve eğlenceli bir konudur. Dilin yapısı içinde gizli bu tür özellikleri keşfetmek, dilbilgisine olan hakimiyeti artırırken aynı zamanda zekice bir oyun sunar.";
-
-
-$palindrom = new Palindrom();
-
-echo json_encode($palindrom->palindrom_paragraf($paragraf), JSON_UNESCAPED_UNICODE);
 ?>
 
 
